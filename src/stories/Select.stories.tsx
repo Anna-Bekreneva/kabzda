e1c9f9c01db19c98a3c50dc7a1c9dae69a46793e
Anna-Bekreneva <anjabekreneva@gmail.com>
2022-11-28 12:20:57 +0300

    added hw (select)

--- src/stories/Select.stories.tsx
+++ src/stories/Select.stories.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState} from 'react';
+import '../components/Select/select.css';
 
 import {ComponentMeta} from '@storybook/react';
 import {Select} from '../components/Select/Select';
@@ -10,13 +11,28 @@ export default {
 
 export const SelectBase = () => {
 	const items = [
-		{title: 'Anna', value: 1}, {title: 'Anka', value: 1}
-	]
+		{title: 'PC', value: 1},
+		{title: 'Table', value: 2},
+		{title: 'Phone', value: 3},
+		{title: 'Notebook', value: 4}
+	];
+
+	const [value, setValue] = useState<string>('Choose a device');
+	const [isOpen, setIsOpen] = useState<boolean>(false)
+
+	const onClickHandler = () => setIsOpen(!isOpen)
+
+	const test = (title: string) => () => {
+		setValue(title);
+		onClickHandler()
+	}
 
 	return (
-		<div>
-            <div>{}</div>
-			{items.map((i => <div>{i.title}</div>))}
+		<div className="select">
+            <div onClick={onClickHandler}>{value}</div>
+			<div className={isOpen ? "select__box select__box--open" : "select__box"}>
+				{items.map(( i => <div onClick={test(i.title)}>{i.title}</div> ))}
+			</div>
         </div>
-	)
-}
+	);
+};
