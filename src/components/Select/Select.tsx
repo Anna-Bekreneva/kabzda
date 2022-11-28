a93865c473717694f9c9880eb332fa01e1373f1c
Anna-Bekreneva <anjabekreneva@gmail.com>
2022-11-28 14:01:58 +0300

    added lesson 15 Select component

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, {useState, KeyboardEvent, useEffect} from 'react';
+import styles from './select.module.css';
 
 export type SelectPropsType = {
-	value: any
-	onClick: (value: any) => void
+	value?: any
+	onChange: (value: any) => void
 	items: Array<ItemType>
-	//onClick: (value: any) => void
 }
 
 type ItemType = {
@@ -12,13 +12,61 @@ type ItemType = {
 	value: any
 }
 
-export function Select(props: SelectPropsType) {
+export function Select (props: SelectPropsType) {
 
+	const [active, setActive] = useState(false);
+	const [hoveredElementValue, setHoveredElementValue] = useState(props.value);
+
+	const selectedItem = props.items.find(i => i.value === props.value);
+	const hoveredItem = props.items.find(i => i.value === hoveredElementValue);
+
+	useEffect(() => {
+		setHoveredElementValue(props.value);
+	}, [props.value]);
+
+	const toggleItems = () => setActive(!active);
+
+	const onItemClick = (value: any) => {
+		props.onChange(value);
+		toggleItems();
+	};
+
+	const onKeyUp = (event: KeyboardEvent<HTMLDivElement>) => {
+		if (event.key === 'ArrowDown' || event.key === 'ArrowUp') {
+			for (let i = 0; i < props.items.length; i++) {
+				if (props.items[i].value === hoveredElementValue) {
+					const pretendentElement = event.key === 'ArrowDown' ? props.items[i + 1] : props.items[i - 1];
+					if (pretendentElement) {
+						props.onChange(pretendentElement.value);
+						return;
+					}
+				}
+			}
+			if (!selectedItem) {
+				props.onChange(props.items[0].value);
+			}
+		}
+
+		if (event.key === 'Enter' || event.key === 'Escape') {
+			setActive(false);
+		}
+	};
 
 	return (
-		<div>
-            <div>{}</div>
-			{props.items.map((i => <div>{i.title}</div>))}
-        </div>
-	)
+		<div onKeyUp={onKeyUp} tabIndex={1}>
+			<span className={styles.main} onClick={toggleItems}>
+				{selectedItem && selectedItem.title}
+			</span> {
+			active &&
+            <div className={styles.items}>
+					{props.items.map(i => <div onMouseEnter={() => {
+						setHoveredElementValue(i.value);
+					}} className={styles.item + ' ' + ( hoveredItem === i ? styles.selected : '' )} onClick={() => onItemClick(i.value)} key={i.value}>
+						{i.title}
+					</div>)}
+				</div>
+		}
+		</div>
+
+	);
 }
--- src/stories/Select.stories.tsx
+++ src/stories/Select.stories.tsx
@@ -3,36 +3,63 @@ import '../components/Select/select.css';
 
 import {ComponentMeta} from '@storybook/react';
 import {Select} from '../components/Select/Select';
+import {action} from '@storybook/addon-actions';
 
 export default {
 	title: 'select',
 	component: Select
 } as ComponentMeta<typeof Select>;
 
-export const SelectBase = () => {
+export const SelectBaseHomeWork = () => {
 	const items = [
-		{title: 'PC', value: 1},
-		{title: 'Table', value: 2},
-		{title: 'Phone', value: 3},
-		{title: 'Notebook', value: 4}
+		{title: 'PC', value: '1'},
+		{title: 'Table', value: '2'},
+		{title: 'Phone', value: '3'},
+		{title: 'Notebook', value: '4'}
 	];
 
 	const [value, setValue] = useState<string>('Choose a device');
-	const [isOpen, setIsOpen] = useState<boolean>(false)
+	const [isOpen, setIsOpen] = useState<boolean>(false);
 
-	const onClickHandler = () => setIsOpen(!isOpen)
+	const onClickHandler = () => setIsOpen(!isOpen);
 
 	const test = (title: string) => () => {
 		setValue(title);
-		onClickHandler()
-	}
+		onClickHandler();
+	};
 
 	return (
 		<div className="select">
-            <div onClick={onClickHandler}>{value}</div>
+		    <div onClick={onClickHandler}>{value}</div>
 			<div className={isOpen ? "select__box select__box--open" : "select__box"}>
-				{items.map(( i => <div onClick={test(i.title)}>{i.title}</div> ))}
+				{items.map(( i => <div key={i.value} onClick={test(i.title)}>{i.title}</div> ))}
 			</div>
-        </div>
+		</div>
+	);
+};
+
+export const WithValue = () => {
+
+	const [value, setValue] = useState('2')
+
+	return (
+		<Select value={value} onChange={setValue} items={[
+			{value: '1', title: 'Minsk'},
+			{value: '2', title: 'Moscow'},
+			{value: '3', title: 'Kiev'}
+		]}/>
+	);
+};
+
+export const WithoutValue = () => {
+
+	const [value, setValue] = useState(null)
+
+	return (
+		<Select value={value} onChange={setValue} items={[
+			{value: '1', title: 'Minsk'},
+			{value: '2', title: 'Moscow'},
+			{value: '3', title: 'Kiev'}
+		]}/>
 	);
 };
